83502f83cbbedca4f039db92349d8d6831828acb
jessie1992 <jessie1992a@gmail.com>
2018-12-27 17:51:41 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <vtkAutoInit.h>
+#include "vtkConeSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderWindow.h"
+#include "vtkCamera.h"
+#include "vtkActor.h"
+#include "vtkRenderer.h"
+
+
+int main() {
+	VTK_MODULE_INIT(vtkRenderingOpenGL2);
+	VTK_MODULE_INIT(vtkInteractionStyle);
+
+
+	vtkConeSource *cone = vtkConeSource::New();
+	cone->SetHeight(3.0);
+	cone->SetRadius(1.0);
+	cone->SetResolution(10);
+	
+	vtkPolyDataMapper *coneMapper = vtkPolyDataMapper::New();
+	coneMapper->SetInputConnection(cone->GetOutputPort());
+
+
+	vtkActor *coneActor = vtkActor::New();
+	coneActor->SetMapper(coneMapper);
+
+	vtkRenderer *ren1 = vtkRenderer::New();
+	ren1->AddActor(coneActor);
+	ren1->SetBackground(0.1, 0.2, 0.4);
+
+
+	vtkRenderWindow *renWin = vtkRenderWindow::New();
+	renWin->AddRenderer(ren1);
+	renWin->SetSize(300, 300);
+
+	int i;
+	for (i = 0; i < 360; ++i)
+	{
+		// render the image
+		renWin->Render();
+		// rotate the active camera by one degree
+		ren1->GetActiveCamera()->Azimuth(1);
+	}
+
+
+	cone->Delete();
+	coneMapper->Delete();
+	coneActor->Delete();
+	ren1->Delete();
+	renWin->Delete();
+
+	system("pause");
+	return 0;
+
+}
